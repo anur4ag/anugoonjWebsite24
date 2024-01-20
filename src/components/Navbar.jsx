533a63edb08f4eec3cba465647631947b040a40f
tarunguvi <tarunsinghwap7@gmail.com>
2024-01-20 22:09:15 +0530

    fixed navbar and home

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,47 +1,61 @@
 import logo from "../assets/logo.svg";
 const leftNavLinks = [
-    {
-        title: 'Home',
-        path: '/'
-    },
-    {
-        title: 'Events',
-        path: '/events'
-    },
-]
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Events",
+    path: "/events",
+  },
+];
 const rightNavLinks = [
-    {
-        title: 'Sponsors',
-        path: '/sponsors'
-    },
-    {
-        title: 'Glimpses',
-        path: '/glimpses'
-    }
-]
+  {
+    title: "Sponsors",
+    path: "/sponsors",
+  },
+  {
+    title: "Glimpses",
+    path: "/glimpses",
+  },
+];
 
 const Navbar = () => {
-    return (
-        <div>
-            <div className="flex items-center py-4 px-8 w-full z-10 justify-around  relative">
-
-                <div className=" md:flex items-center space-x-5">
-                    {leftNavLinks.map((link, index) => (
-                        <a href={link.path} key={index} className="text-lg font-semibold text-navtext transition duration-300">{link.title}</a>
-                    ))}
-                </div>
-                <div className="">
-
-                    <img src={logo} alt="logo" />
-                </div>
-                <div className=" md:flex items-center space-x-5">
-                    {rightNavLinks.map((link, index) => (
-                        <a href={link.path} key={index} className="text-lg font-semibold text-navtext  transition duration-300">{link.title}</a>
-                    ))}
-                </div>
-            </div>
+  return (
+    <div>
+      <div className="container mx-auto flex items-center py-4 px-8 w-full z-10 justify-evenly relative">
+        <div className=" md:flex items-center space-x-5 gap-20">
+          {leftNavLinks.map((link, index) => (
+            <a
+              href={link.path}
+              key={index}
+              className="text-lg font-semibold text-navtext uppercase transition duration-300"
+            >
+              {link.title}
+            </a>
+          ))}
         </div>
-    )
-}
+        <div className="relative bottom-none md:-bottom-16 lg:-bottom-16">
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <div className="hidden lg:block absolute right-[22em] w-[28.5em] h-0.5 bg-[#1E1E1E]" />
+            <img src={logo} alt="logo" className="w-[10em] md:w-[18em] lg:w-[20em]" />
+            <div className="hidden lg:block absolute left-[22em] w-[28.5em] h-0.5 bg-[#1E1E1E]" />
+          </div>
+        </div>
+        <div className=" md:flex items-center space-x-5 gap-20">
+          {rightNavLinks.map((link, index) => (
+            <a
+              href={link.path}
+              key={index}
+              className="text-lg font-semibold text-navtext uppercase transition duration-300"
+            >
+              {link.title}
+            </a>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,38 @@
-import background from '../assets/background.svg'
-import top from "../assets/top.svg"
-import bottom from "../assets/bottom.svg"
-import bottomIllustration from "../assets/bottomillustrations.svg"
-import Navbar from '../components/Navbar'
+import background from "../assets/background.svg";
+import top from "../assets/top.svg";
+import bottom from "../assets/bottom.svg";
+import bottomIllustration from "../assets/bottomillustrations.svg";
+import Navbar from "../components/Navbar";
 const Home = () => {
-    return (
-        <>
-            <div className="min-h-screen min-w-screen relative" style={{
-                backgroundImage: `url(${background})`,
-                backgroundRepeat: "no-repeat",
-                backgroundSize: "cover",
-                backgroundPosition: "center",
-            }}>
-                <Navbar />
-                <div className="absolute -top-20 left-0 ">
-                    <img src={top} alt="top" />
-                </div>
-                <div className='flex absolute top-[40%] justify-center items-center w-full'>
-                    <h1 className='text-center text-5xl lg:text-9xl z-10 uppercase text-navtext font-bold'>Comming Soon</h1>
-                </div>
-                <div className="absolute bottom-0 left-0">
-                    <img src={bottom} alt="bottom" />
-                </div>
-                <div className="absolute bottom-0 -left-8 ">
-                    <img src={bottomIllustration} alt="bottomIllustration" />
-                </div>
+  return (
+    <>
+      <div
+        className="min-h-screen min-w-screen relative"
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundRepeat: "no-repeat",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+      >
+        <Navbar />
+        <div className="relative flex justify-center -top-[10em]">
+          <img src={top} alt="top" />
+        </div>
+        <div className="flex absolute top-[40%] justify-center items-center w-full">
+          <h1 className="text-center text-5xl lg:text-9xl z-10 uppercase text-navtext font-bold">
+            Comming Soon
+          </h1>
+        </div>
+        <div className="relative flex justify-center bottom-0 left-0">
+          <img src={bottom} alt="bottom" />
+        </div>
+        <div className="relative bottom-0 -left-8 ">
+          <img src={bottomIllustration} alt="bottomIllustration" />
+        </div>
+      </div>
+    </>
+  );
+};
 
-            </div>
-        </>
-    )
-}
-
-export default Home
+export default Home;
